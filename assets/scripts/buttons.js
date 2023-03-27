de31e4909f57b2ebc84c487b41b2461f9611f30d
josephiskingkong <iloveuyungcole@gmail.com>
2023-03-27 14:25:26 +0700

    I hide the scrollbar

--- assets/scripts/buttons.js
+++ assets/scripts/buttons.js
@@ -19,7 +19,6 @@ const trackListCovers = document.getElementsByClassName('tracklist-cover')
 const trackListArtists = document.getElementsByClassName('track-author-tracklist')
 const trackListSongNames = document.getElementsByClassName('track-name-tracklist')
 const trackText = document.getElementById('lyrics-header')
-const trackText = document.getElementById('lyrics-header')
 const currentCover = document.getElementsByClassName('track-cover')
 const currentArtist = document.getElementsByClassName('track-author')
 const currentSongName = document.getElementsByClassName('track-name')
@@ -28,13 +27,11 @@ const currentBackground = document.getElementsByClassName('background')
 const songsPool = new ObjectPool(6, createDecoratedSong);
 const magnolia = songsPool.getObject('Magnolia', 'Playboi Carti', ".\\assets\\tracks\\Playboi_Carti_Magnolia.mp3", {text: ".\\assets\\texts\\Magnolia.txt"})
 const oddity = songsPool.getObject('Space Oddity', 'David Bowie', ".\\assets\\tracks\\David_Bowie_Space_Oddity.mp3", {text: ".\\assets\\texts\\Space Oddity.txt"})
-const magnolia = songsPool.getObject('Magnolia', 'Playboi Carti', ".\\assets\\tracks\\Playboi_Carti_Magnolia.mp3", {text: ".\\assets\\texts\\Magnolia.txt"})
-const oddity = songsPool.getObject('Space Oddity', 'David Bowie', ".\\assets\\tracks\\David_Bowie_Space_Oddity.mp3", {text: ".\\assets\\texts\\Space Oddity.txt"})
 const man = songsPool.getObject('Only Man', 'Audio Bullys', ".\\assets\\tracks\\Audio_Bullys_Only_Man.mp3")
 const immigrant = songsPool.getObject('Immigrant Song', 'Led Zeppelin', ".\\assets\\tracks\\Led_Zeppelin_Immigrant_Song.mp3")
 const rocket = songsPool.getObject('Silver Rocket', 'Sonic Youth', ".\\assets\\tracks\\Sonic_Youth_Silver_Rocket.mp3")
-const alarm = songsPool.getObject('False Alarm', 'The Weeknd', ".\\assets\\tracks\\The_Weeknd_False_Alarm.mp3")
-const nostylist = songsPool.getObject('NO STYLIST', 'Destroy Lonely', ".\\assets\\tracks\\Destroy Lonely - NOSTYLIST (Official Audio).mp3")
+const alarm = songsPool.getObject('False Alarm', 'The Weeknd', ".\\assets\\tracks\\The_Weeknd_False_Alarm.mp3", {text: ".\\assets\\texts\\False Alarm.txt"})
+const nostylist = songsPool.getObject('NO STYLIST', 'Destroy Lonely', ".\\assets\\tracks\\Destroy Lonely - NOSTYLIST (Official Audio).mp3", {text: ".\\assets\\texts\\NO STYLIST.txt"})
 const wannarock = songsPool.getObject('I Just Wanna Rock', 'Lil Uzi Vert', ".\\assets\\tracks\\Lil Uzi Vert - Just Wanna Rock [Official Visualizer].mp3")
 const grid = songsPool.getObject('Off The Grid (Remix)', 'Playboi Carti', ".\\assets\\tracks\\Playboi Carti - Off the Grid (Remix) (Tiktok Viral Song).mp3")
 const cooler = songsPool.getObject('Cooler Than Me', 'Lancey Foux', ".\\assets\\tracks\\Lancey Foux - Cooler Than Me (Visualiser).mp3")
@@ -42,7 +39,6 @@ const cooler = songsPool.getObject('Cooler Than Me', 'Lancey Foux', ".\\assets\\
 let currentSongIndex = 0
 let songsAmount = 10
 let paused = false
-let newSongIndex = Number
 let newSongIndex = Number
 
 const playlist = {
@@ -79,7 +75,6 @@ const playlist = {
             while (newSongIndex === currentSongIndex || newSongIndex === -1) {
                 newSongIndex = Math.floor(Math.random() * (songsAmount))
             }
-            alert(newSongIndex)
             metaConfigure(currentSongIndex, newSongIndex)
             if (!paused) {
                 playlist[currentSongIndex].play()
@@ -101,7 +96,6 @@ const playlist = {
                 playlist[currentSongIndex].play()
                 changeButtons()
             }
-            }
             return { done, value: playlist[currentSongIndex] } 
         }
     },
@@ -118,7 +112,6 @@ const playlist = {
             while (newSongIndex === currentSongIndex || newSongIndex === -1) {
                 newSongIndex = Math.floor(Math.random() * (songsAmount))
             }
-            alert(newSongIndex)
         } else if (currentSongIndex === 0) {
             newSongIndex = songsAmount- 1
         } else {
@@ -144,29 +137,12 @@ function getTextFromFile(filepath) {
     }
 }
 
-function getTextFromFile(filepath) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', filepath, false);
-    xhr.send();
-  
-    if (xhr.status === 200) {
-      return xhr.responseText;
-    } else {
-      throw new Error('Error loading file');
-    }
-}
-
 function metaConfigure(oldIndex, newIndex) {
     playlist[currentSongIndex].pause()
     playlist[currentSongIndex].audio.currentTime = 0
 
     if (replayButton.classList.contains('static-replay')) {
-    playlist[currentSongIndex].pause()
-    playlist[currentSongIndex].audio.currentTime = 0
-
-    if (replayButton.classList.contains('static-replay')) {
         playlist[oldIndex].audio.loop = false
-        playlist[newIndex].audio.loop = true
         playlist[newIndex].audio.loop = true
     }
     currentSongIndex = newIndex
@@ -181,12 +157,6 @@ function metaConfigure(oldIndex, newIndex) {
         trackText.innerHTML = playlist[currentSongIndex].title + "<br>" + playlist[currentSongIndex].artist
     }
 
-    if (playButton.classList.contains('hidden') && !playlist[newIndex].paused) {
-    currentCover[0].src = trackListCovers[newIndex].src
-    currentArtist[0].textContent = trackListArtists[newIndex].textContent
-    currentSongName[0].textContent = trackListSongNames[newIndex].textContent
-    currentBackground[0].style.backgroundImage = 'url(' + trackListCovers[newIndex].src + ')'
-    
     if (playlist[currentSongIndex].text !== undefined) {
         trackText.innerHTML =  getTextFromFile(playlist[currentSongIndex].text)
     } else {
@@ -198,14 +168,7 @@ function metaConfigure(oldIndex, newIndex) {
     }
 }
 
-playlist[0].audio.addEventListener('timeupdate', () => {
-    const currentTime = playlist[currentSongIndex].audio.currentTime
-    const duration = playlist[currentSongIndex].audio.duration
-    const progress = (currentTime / duration) * 100
-    trackBar.value = progress
-})
-playlist[0].audio.addEventListener("ended", () => {
-    if (!replayButton.classList.contains('static-replay')) {
+
 playlist[0].audio.addEventListener('timeupdate', () => {
     const currentTime = playlist[currentSongIndex].audio.currentTime
     const duration = playlist[currentSongIndex].audio.duration
@@ -217,11 +180,7 @@ playlist[0].audio.addEventListener("ended", () => {
         playlist.next()
     }
     playlist[currentSongIndex].play()
-}) 
-for (let element of playlist) {
-    }
-    playlist[currentSongIndex].play()
-}) 
+})
 for (let element of playlist) {
     element.audio.addEventListener('timeupdate', () => {
         const currentTime = playlist[currentSongIndex].audio.currentTime
@@ -240,13 +199,7 @@ for (let element of playlist) {
             playlist.next()
         }
         playlist[currentSongIndex].play()
-    }) 
-    element.audio.addEventListener("ended", () => {
-        if (!replayButton.classList.contains('static-replay')) {
-            playlist.next()
-        }
-        playlist[currentSongIndex].play()
-    }) 
+    })
 }
 
 forwardButton.addEventListener('click', function() {
@@ -258,7 +211,6 @@ backButton.addEventListener('click', function() {
 })
 
 shuffleButton.addEventListener('click', function() {
-    shuffleButton.classList.toggle('static')
     shuffleButton.classList.toggle('static')
 })
 
@@ -291,15 +243,8 @@ window.addEventListener('keypress', function (event) {
         playlist.next()
     } else if (event.key === 'j') {
         shuffleButton.classList.toggle('static')
-        shuffleButton.classList.toggle('static')
     } else if (event.key === ';') {
         replayButton.classList.toggle('static-replay')
-        if (playlist[currentSongIndex].audio.loop) {
-            playlist[currentSongIndex].audio.loop = false
-        } else {
-            playlist[currentSongIndex].audio.loop = true
-        }
-    }
         if (playlist[currentSongIndex].audio.loop) {
             playlist[currentSongIndex].audio.loop = false
         } else {
@@ -328,12 +273,6 @@ mobileVolumeBar.addEventListener('input', () => {
 })
 
 replayButton.onclick = function () {
-    if (replayButton.classList.contains('static-replay')) {
-        playlist[currentSongIndex].audio.loop = false
-        
-    } else {
-        playlist[currentSongIndex].audio.loop = true
-    }
     if (replayButton.classList.contains('static-replay')) {
         playlist[currentSongIndex].audio.loop = false
         
